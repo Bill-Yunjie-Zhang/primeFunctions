14df60bf35eb639566339bf651e80d689a020492
Bill <207559339@qq.com>
2020-02-17 18:35:00 +0800

    check if a number is prime

---
+++ checkPrime.js
@@ -0,0 +1,9 @@
+const checkPrime = (num) => {
+    for (ii = 2; ii < num; ii ++){
+        if (num % ii === 0) {
+            return false
+        }
+    }
+    return true
+}
+console.log(checkPrime(541))
