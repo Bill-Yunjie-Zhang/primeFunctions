3b164c9223177b9e1ba8df85fc5f8b5ea44d6052
Bill <207559339@qq.com>
2020-02-18 11:04:19 +0800

    0 and 1 are not prime

--- checkPrime.js
+++ checkPrime.js
@@ -1,4 +1,7 @@
 const checkPrime = (num) => {
+    if(num === 1 || num === 0) {
+        return false
+    }
     for (ii = 2; ii < num; ii ++){
         if (num % ii === 0) {
             return false
--- getRandomPrime.js
+++ getRandomPrime.js
@@ -12,9 +12,7 @@ const getRandomPrime = (low, high) => {
 }
 
 // console.log(getRandomPrime(7, 10))
-// for(ii = 0; ii < 100; ii ++){
-//     let a = getRandomPrime(1, 100)
-//     if(a === 9){
-//         console.log(a + " oh yeah")
-//     }
-// }
+
+for(zz = 0; zz < 10; zz ++){
+    console.log(zz + ": " + getRandomPrime(0, 10))
+}
