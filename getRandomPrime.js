7496e02f6dfc0ab6988fb06870eb396785d4ce25
Bill <207559339@qq.com>
2020-02-18 10:54:25 +0800

    fixed a bug

--- getRandomPrime.js
+++ getRandomPrime.js
@@ -7,8 +7,14 @@ const getRandomPrime = (low, high) => {
     if (b) {
         return num
     } else {
-        return getRandomNum(low, high)
+        return getRandomPrime(low, high)
     }
 }
 
-console.log(getRandomPrime(1, 10000000))
+// console.log(getRandomPrime(7, 10))
+// for(ii = 0; ii < 100; ii ++){
+//     let a = getRandomPrime(1, 100)
+//     if(a === 9){
+//         console.log(a + " oh yeah")
+//     }
+// }
